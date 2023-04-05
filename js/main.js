44f73a6484c33ebc6d31842dc3cbbce8c33b2b7a
Gabriele31201 <gabri31201@gmail.com>
2023-04-05 16:20:45 +0200

    add userWord and ciclo for

---
+++ js/main.js
@@ -0,0 +1,30 @@
+//inizzializzare la parola dell'utente
+
+let userWord = "Ciao"
+
+// scomporre la parole
+for(i = 0; i < userWord.length; i++){
+    console.log(userWord.charAt(i));
+}
+
+// ricomporla al contrario
+let paliUserWord = userWord
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//----------------------------------- funzioni -----------------------------
+
+
+
